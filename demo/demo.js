a671be87e7348341342b11524a2a892fa48d3842
Elijah Insua <tmpvar@gmail.com>
2014-12-12 00:42:12 -0800

    convert unproject to take a z, and simplify mouse to vec2

--- demo/demo.js
+++ demo/demo.js
@@ -16,8 +16,8 @@ var getEye = require('eye-vector');
 var aabb = require('./util/aabb');
 var mouse = {
   down: false,
-  pos: [0, 0, 0],
-  world: [0, 0, 0]
+  pos: [0, 0],
+  far: [0, 0, 0]
 };
 var eye = [0, 0, 0];
 
@@ -258,11 +258,11 @@ function handleMouse(e) {
 
       mouse.pos[0] = x;
       mouse.pos[1] = y;
-      mouse.pos[2] = 1;
 
       var rayDirection = vec3.unproject(
-        [0, 0, 0],
+        mouse.far,
         mouse.pos,
+        1,
         view,
         projection,
         scene.viewport
--- demo/util/vec3-unproject.js
+++ demo/util/vec3-unproject.js
@@ -7,14 +7,14 @@ var m4scratch = mat4.create();
 var m4scratch2 = mat4.create();
 var v3scratch = [0, 0, 0];
 
-vec3.unproject = function (dest, vec, view, proj, viewport) {
+vec3.unproject = function (dest, vec, z, view, proj, viewport) {
   if(!dest) {
     dest = [0, 0, 0];
   }
 
   dest[0] = 2.0 * (vec[0] - viewport[0]) / viewport[2] - 1.0;
   dest[1] = 2.0 * (vec[1] - viewport[1]) / viewport[3] - 1.0;
-  dest[2] = 2.0 * vec[2] - 1.0;
+  dest[2] = 2.0 * z - 1.0;
 
   mat4.multiply(m4scratch, proj, view);
   vec3.transformMat4(dest, dest, mat4.invert(m4scratch, m4scratch));
