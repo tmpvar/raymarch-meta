cc7bcf76ddde49e1e1a804a01dc7149cade16974
Elijah Insua <tmpvar@gmail.com>
2014-12-11 23:53:25 -0800

    cleanup for vec3-unproject

--- demo/util/vec3-unproject.js
+++ demo/util/vec3-unproject.js
@@ -12,12 +12,12 @@ vec3.unproject = function (dest, vec, view, proj, viewport) {
     dest = [0, 0, 0];
   }
 
-  v3scratch[0] = (vec[0] - viewport[0]) * 2.0 / viewport[2] - 1.0;
-  v3scratch[1] = (vec[1] - viewport[1]) * 2.0 / viewport[3] - 1.0;
-  v3scratch[2] = 2.0 * vec[2] - 1.0;
+  dest[0] = 2.0 * (vec[0] - viewport[0]) / viewport[2] - 1.0;
+  dest[1] = 2.0 * (vec[1] - viewport[1]) / viewport[3] - 1.0;
+  dest[2] = 2.0 * vec[2] - 1.0;
 
-  mat4.multiply(m4scratch, view, mat4.invert(m4scratch2, proj));
-  vec3.transformMat4(dest, v3scratch, m4scratch);
+  mat4.multiply(m4scratch, proj, view);
+  vec3.transformMat4(dest, dest, mat4.invert(m4scratch, m4scratch));
 
   return dest;
 };
