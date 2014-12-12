83581d78b525838ce44fae22d0f2006bd22a103a
Elijah Insua <tmpvar@gmail.com>
2014-12-12 01:52:20 -0800

    fix copy/pasta

--- demo/demo.js
+++ demo/demo.js
@@ -39,10 +39,10 @@ var v3scratch = [0, 0, 0];
 var m4scratch = mat4.create();
 
 function getEye(out, view) {
-  mat4.invert(m4scratch, viewMatrix);
-  out[0] = scratch[12];
-  out[1] = scratch[13];
-  out[2] = scratch[14]
+  mat4.invert(m4scratch, view);
+  out[0] = m4scratch[12];
+  out[1] = m4scratch[13];
+  out[2] = m4scratch[14]
   return out;
 }
 
