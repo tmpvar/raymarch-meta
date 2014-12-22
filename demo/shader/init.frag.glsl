87e3210b84168dd79ac30701b62d02849298e8d2
Elijah Insua <tmpvar@gmail.com>
2014-12-22 00:00:46 -0800

    colorize init for now for debug

--- demo/shader/init.frag.glsl
+++ demo/shader/init.frag.glsl
@@ -2,19 +2,9 @@
 precision highp float;
 #endif
 
-uniform sampler2D ops;
-uniform sampler2D fbo;
-uniform mat4 clipToWorld;
-uniform mat4 worldToClip;
-uniform vec2 resolution;
-uniform float time;
-
-uniform vec3 camera_eye;
-uniform float camera_distance;
-
 varying vec3 v_uv;
-
+uniform vec2 resolution;
 
 void main() {
-  gl_FragColor = vec4(1.0);
+  gl_FragColor = normalize(vec4(.5 * normalize(v_uv.xy)  + 1.0, 0.0, 1.0));
 }
