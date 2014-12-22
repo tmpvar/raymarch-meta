df8f95a0b5ea28c7e34bd6f660aa314e6031ff2f
Elijah Insua <tmpvar@gmail.com>
2014-12-21 18:07:28 -0800

    debug frag just writes the contents of the last fb on the screen

--- demo/shader/debug.frag.glsl
+++ demo/shader/debug.frag.glsl
@@ -1,6 +1,9 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
 uniform sampler2D fbo;
-uniform vec2 fbo_resolution;
 
 void main() {
-  gl_FragColor = texture2D(fbo, gl_FragCoord.xy);
+  gl_FragColor = texture2D(fbo, 0.5 * (gl_FragCoord.xy + 1.0));
 }
