6f3a8bfb5f2d486e96140b04c168355e6ceb14da
Elijah Insua <tmpvar@gmail.com>
2014-12-21 23:57:55 -0800

    compute uv coords in vert

--- demo/shader/vert.glsl
+++ demo/shader/vert.glsl
@@ -13,8 +13,8 @@ uniform vec2 resolution;
 
 
 void main() {
-  v_uv = position;
-  //gl_Position = worldToClip * vec4(position, 1.0);
+  v_uv = 0.5 * (position+1.0);
+
   gl_Position = vec4(position, 1.0);
 
   float aspect = resolution.x/resolution.y;
