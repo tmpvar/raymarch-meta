4a3d180a4459c0299610ffaafca7920f4b18769a
Elijah Insua <tmpvar@gmail.com>
2014-12-31 17:29:21 -0800

    tune cone march

--- demo/shader/depth.frag.glsl
+++ demo/shader/depth.frag.glsl
@@ -24,7 +24,7 @@ varying float v_fov;
 #define OPS_SIZE /* OPS_SIZE */
 #define OPS_RATIO 1.0//* OPS_SIZE */
 
-#define RAYMARCH_CYCLES 32 /* RAYMARCH_DISABLED_CYCLES */
+#define RAYMARCH_CYCLES 128 /* RAYMARCH_DISABLED_CYCLES */
 #define RAYMARCH_EPS 0.0001
 
 float sample(int x, int y) {
@@ -56,10 +56,11 @@ vec3 perform_selection(in vec3 color, in float val) {
   return mix(color, selectionColor, val);
 }
 
-float raymarch(in vec3 origin, in vec3 direction, in float initial_dist) {
-  float dist = 0.0;
+vec4 raymarch(in vec3 origin, in vec3 direction, in float dist) {
   float h = 1.0;
+  float hit = 0.0;
   vec4 pos4;
+  float m = (resolution.x/resolution.y);
 
 /* RAYMARCH_SETUP */
 
@@ -71,45 +72,62 @@ float raymarch(in vec3 origin, in vec3 direction, in float initial_dist) {
 /* RAYMARCH_OPS */
 
     // break if the size of the sphere is smaller than the radius of the cone
-    float cone_radius = v_fov * dist;
-    if (dist + initial_dist < cone_radius || h < RAYMARCH_EPS) {
+    float latest_distance = h;// + dist;
+    float cone_radius = v_fov * latest_distance;
+    if (h/2.0 < cone_radius || h < RAYMARCH_EPS) {
+      hit = 1.0;
+      dist -= h;
       break;
     }
-
 
     dist += h;
   }
 
-  return dist;
+  return vec4(dist, hit, 1.0, 0.0);
 }
 
 
 // TODO: support implementations/cards without OES_texture_float
 void main() {
   float m = 1.0 / max(resolution.x, resolution.y);
-  vec3 eye = clipToWorld[3].xyz / clipToWorld[3].w;
 
-  float c  = texture2D(fbo, v_uv.xy).x;
-  float ul = texture2D(fbo, v_uv.xy + vec2(-m,  m)).x;
-  float t  = texture2D(fbo, v_uv.xy + vec2( 0.0,  m)).x;
-  float ur = texture2D(fbo, v_uv.xy + vec2( m)).x;
-  float r  = texture2D(fbo, v_uv.xy + vec2( m,  0.0)).x;
-  float br = texture2D(fbo, v_uv.xy + vec2( m, -m)).x;
-  float b  = texture2D(fbo, v_uv.xy + vec2( 0.0, -m)).x;
-  float bl = texture2D(fbo, v_uv.xy + vec2(-m)).x;
-  float l  = texture2D(fbo, v_uv.xy + vec2(-m,  0.0)).x;
+  vec4 last_sample  = texture2D(fbo, v_uv.xy);
+  // float ul = texture2D(fbo, v_uv.xy + vec2(-m,  m)).x;
+  // float t  = texture2D(fbo, v_uv.xy + vec2( 0.0,  m)).x;
+  // float ur = texture2D(fbo, v_uv.xy + vec2( m)).x;
+  // float r  = texture2D(fbo, v_uv.xy + vec2( m,  0.0)).x;
+  // float br = texture2D(fbo, v_uv.xy + vec2( m, -m)).x;
+  // float b  = texture2D(fbo, v_uv.xy + vec2( 0.0, -m)).x;
+  // float bl = texture2D(fbo, v_uv.xy + vec2(-m)).x;
+  // float l  = texture2D(fbo, v_uv.xy + vec2(-m,  0.0)).x;
 
-  float last_distance = min(c, min(ul, min(t, min(ur, min(r, min(br, min(b, min(bl, l))))))));
-  if (last_distance < 1000.0) {
+  // float last_distance = min(c, min(ul, min(t, min(ur, min(r, min(br, min(b, min(bl, l))))))));
 
-    eye = eye + v_dir * last_distance;
+  if (last_sample.y == 1.0) {
 
-    float surface_distance = 0.0;
-    surface_distance = raymarch(eye, v_dir, last_distance);
-
-
-    gl_FragColor = vec4(surface_distance, 0.0, 0.0, 1.0);
+    gl_FragColor = raymarch(
+      clipToWorld[3].xyz / clipToWorld[3].w,
+      v_dir,
+      last_sample.x
+    );
   } else {
-    gl_FragColor = vec4(last_distance, 0.0, 0.0, 1.0);
+    gl_FragColor = last_sample;
   }
+
+  // gl_FragColor = mix(
+  //   last_sample,
+  //   current_sample,
+  //   current_sample.y
+  // );
+
+  // if (last_sample.y == 1.0) {
+
+
+  //   gl_FragColor = r
+
+
+  //   // gl_FragColor = vec4(surface_distance, hit, 0.0, 1.0);
+  // } else {
+  //   gl_FragColor = last_sample;//vec4(last_sample.x, 0.0, 0.0, 1.0);
+  // }
 }
