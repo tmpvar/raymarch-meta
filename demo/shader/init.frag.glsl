96de4dc69e529929bbeb15e3291b33d4842b4299
Elijah Insua <tmpvar@gmail.com>
2014-12-31 17:23:51 -0800

    make init shader emit solid green

--- demo/shader/init.frag.glsl
+++ demo/shader/init.frag.glsl
@@ -6,5 +6,5 @@ varying vec3 v_uv;
 uniform vec2 resolution;
 
 void main() {
-  gl_FragColor = vec4(normalize(v_uv.xy), 0.0, 1.0);
+  gl_FragColor = vec4(0.0, 1.0, 0.0, 0.0);//vec4(normalize(v_uv.xy), 0.0, 1.0);
 }
